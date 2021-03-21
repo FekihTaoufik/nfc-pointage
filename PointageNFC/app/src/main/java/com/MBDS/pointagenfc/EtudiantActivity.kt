d2a0e2b769fe7d643aabce550d4c8ac240de5767
Diallo-ucad <demfoula@outlook.com>
2021-03-21 14:21:05 +0100

    add: Android App

---
+++ PointageNFC/app/src/main/java/com/MBDS/pointagenfc/EtudiantActivity.kt
@@ -0,0 +1,11 @@
+package com.MBDS.pointagenfc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EtudiantActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_etudiant)
+    }
+}
